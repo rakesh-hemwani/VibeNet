347ebe38641de5cba6c41fad3f6cf11438a895c3
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2024-11-19 00:22:29 +0530

    Auth Frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
+    <div className="min-h-screen">
      <AuthScreen/>
     </div>
   );
--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -10,7 +10,7 @@ export const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>("signIn");
 
     return (
-    <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+    <div className="h-full min-h-screen flex items-center justify-center bg-[#5C3B58]">
         <div className="md:h-auto md:w-[420px]">
             {state === 'signIn' ? <SignInCard/> : <SignUpCard/>}
         </div> 
